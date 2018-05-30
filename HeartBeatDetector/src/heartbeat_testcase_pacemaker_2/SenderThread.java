616279ebe3830eb4e0dc5e4644fe695ee22695a0
Ananta Kumar Das <ananta.iiitb@gmail.com>
2018-05-30 14:36:28 +0530

    Modified

--- HeartBeatDetector/src/heartbeat_testcase_pacemaker_2/SenderThread.java
+++ HeartBeatDetector/src/heartbeat_testcase_pacemaker_2/SenderThread.java
@@ -1,6 +1,7 @@
 package heartbeat_testcase_pacemaker_2;
 
-public class SenderThread { 
+public class SenderThread extends Thread{ 
+	
 	public void run() {
 		long currentTime = System.currentTimeMillis(); 
 		new HeartbeatSender(1000).sendHB(currentTime);
